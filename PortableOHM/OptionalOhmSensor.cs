728c955adb1df8348a07d809e8b83ffc9f51e220
Krillsson <contact@christian-jensen.se>
2016-02-13 12:46:18 +0100

    Add OptionalOhmSensor.cs

---
+++ PortableOHM/OptionalOhmSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHMWrapper
+{
+    public class OptionalOhmSensor
+    {
+        private OHMSensor value;
+
+        public OptionalOhmSensor(OHMSensor value)
+        {
+            this.value = value;
+        }
+
+        public bool isPresent()
+        {
+            return value != null;
+        }
+
+        public OHMSensor get()
+        {
+            return value;
+        }
+    }
+}
